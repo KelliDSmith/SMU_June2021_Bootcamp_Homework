0232ee13f0d6998cd9c2d0e35929d1ca4f2d6d1b
Kelli Smith <84884700+KelliDSmith@users.noreply.github.com>
2021-09-29 17:36:37 -0500

    update homework

--- 14 - Interactive Web Visualizations/web visualizations/static/js/app.js
+++ 14 - Interactive Web Visualizations/web visualizations/static/js/app.js
@@ -2,6 +2,11 @@ $(document).ready(function() {
     // alert("Page Loaded");
 
     getData();
+
+    $("#selDataset").on("change",function() {
+        getData();
+    });
+
 });
 
 function getData() {
@@ -14,12 +19,12 @@ function getData() {
         contentType: "application/json",
         dataType: "json",
         success: function(data) {
-
-            // DO WORK HERE
-            console.log(data);
+         
             buildDropdown (data);
             buildBarPlot (data);
             buildBubblePlot(data);
+            buildMetaTable(data);
+            // buildGauge(data);
         },
         error: function(data) {
             console.log("YOU BROKE IT!!");
@@ -57,16 +62,25 @@ function buildBarPlot (data) {
         type: 'bar',
         orientation: 'h',
         marker: {
-            color: 'blue'
+            color: 'maroon'
         }
     }
 
     let traces = [trace1];
 
     let layout = {
+        autosize: false,
+        width: 850,
+        height: 600,
         title: "Bacteria Count in Belly Button",
         xaxis: {
             title: "Number of Bacteria"
+        },
+        yaxis: {
+            tickfont: {
+                size: 10,
+                color: 'black'
+            }
         }
     };
 
@@ -101,3 +115,63 @@ function buildBubblePlot(data) {
       
       Plotly.newPlot('bubble', traces, layout);
 };
+
+// Metadata Table //
+
+function buildMetaTable(data) {
+    let curr_id = parseInt($("#selDataset").val());
+    let curr_data = data.metadata.filter(x => x.id == curr_id)[0];
+
+    //Clear Table
+    $("#sample-metadata").empty();
+
+    let items = Object.entries(curr_data).map(([key, value]) => `${key} : ${value}`)
+    console.log(items)
+
+    for (let i = 0; i < items.length; i++) {
+        let item = items[i];
+        let html_item = `<p">${item}</p>`;
+        $("#sample-metadata").append(html_item);
+    }
+    
+}
+
+// Attempting Gauge Chart //
+
+// function buildGauge(data) {
+//     let curr_id = parseInt($("#selDataset").val());
+//     let curr_data = data.metadata.filter(x => x.id == curr_id)[0];
+
+//     console.log(curr_data.wfreq)
+
+
+//     let data = [
+//         {
+//         type: "indicator",
+//         mode: "gauge+number+delta",
+//         value: curr_data.wfreq,
+//         title: { text: "Belly Button Washing Freqency", font: { size: 24 } },
+//         gauge: {
+//             axis: { range: [null, 10], tickwidth: 1, tickcolor: "darkblue" },
+//             bar: { color: "darkblue" },
+//             bgcolor: "white",
+//             borderwidth: 2,
+//             bordercolor: "gray",
+//         //     steps: [
+//         //     { range: [0, 250], color: "cyan" },
+//         //     { range: [250, 400], color: "royalblue" }
+//         //     ],
+//         // }
+//         }
+//     ];
+    
+//     var layout = {
+//         // width: 500,
+//         // height: 400,
+//         // margin: { t: 25, r: 25, l: 25, b: 25 },
+//         paper_bgcolor: "lavender",
+//         font: { color: "darkblue", family: "Arial" }
+//     };
+    
+//     Plotly.newPlot('gauge', data, layout);
+// }
